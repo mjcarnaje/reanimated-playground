e875fce44727bbbec1fac7293fcc624014d3cfc2
mjcarnaje <michaeljamescarnaje1@gmail.com>
2021-10-27 13:15:17 +0800

    flatlist carousel animation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts } from "expo-font";
+import * as React from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AdvancedFlatListCarousel from "./screens/AdvancedFlatListCarousel";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
+  let [fontsLoaded] = useFonts({
+    "Inter-Thin": require("./assets/fonts/Inter/Inter-Thin.ttf"),
+    "Inter-ExtraLight": require("./assets/fonts/Inter/Inter-ExtraLight.ttf"),
+    "Inter-Light": require("./assets/fonts/Inter/Inter-Light.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter/Inter-Regular.ttf"),
+    "Inter-Medium": require("./assets/fonts/Inter/Inter-Medium.ttf"),
+    "Inter-Semibold": require("./assets/fonts/Inter/Inter-SemiBold.ttf"),
+    "Inter-Bold": require("./assets/fonts/Inter/Inter-Bold.ttf"),
+    "Inter-ExtraBold": require("./assets/fonts/Inter/Inter-ExtraBold.ttf"),
+    "Inter-Black": require("./assets/fonts/Inter/Inter-Black.ttf"),
+  });
+
+  React.useEffect(() => {
+    console.log(fontsLoaded + "...");
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={[
+          StyleSheet.absoluteFillObject,
+          { justifyContent: "center", alignItems: "center" },
+        ]}
+      >
+        <Text>Loading..</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Advanced FlatList Carousel"
+          component={AdvancedFlatListCarousel}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
